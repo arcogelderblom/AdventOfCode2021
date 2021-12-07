b93d4fb51e3c5ea39a262ad7f382fcac997f7ec9
Arco Gelderblom <arcogelderblom@MacBook-Air-van-Arco.local>
2021-12-07 12:25:25 +0100

    Added day 7

---
+++ Day07/day7.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cassert>
+#include <map>
+
+#include "inputfile.hpp"
+#include "stringoperations.hpp"
+#include "vectoroperations.hpp"
+
+int getLeastFuelCommonPosition(std::vector<int> initialStatePositions, bool incrementingFuelCount)
+{
+    int minElement = *std::min_element(initialStatePositions.begin(), initialStatePositions.end());
+    int maxElement = *std::max_element(initialStatePositions.begin(), initialStatePositions.end());
+
+    std::map<int, int> fuelAndCommonPosition;
+
+    for (int position = minElement; position <= maxElement; position++)
+    {
+        int totalFuel = 0;
+        for (int currentPosition : initialStatePositions)
+        {
+            if (incrementingFuelCount)
+            {
+                totalFuel += (std::abs(currentPosition - position) * (std::abs(currentPosition - position) + 1)) / 2;
+            }
+            else
+            {
+                totalFuel += std::abs(currentPosition - position);
+            }
+        }
+        fuelAndCommonPosition[totalFuel] = position;
+    }
+
+    return fuelAndCommonPosition.begin()->first;
+}
+
+int main(void)
+{
+    std::vector<int> initialStatePositionsTest = VectorOperations::stringVectorToIntVector(StringOperations::splitString("16,1,2,0,4,2,7,1,2,14", ","));
+    assert(getLeastFuelCommonPosition(initialStatePositionsTest, false) == 37);
+
+    InputFile inputFile("InputFiles/day07.txt");
+    std::vector<int> initialStatePositions = inputFile.getContentAsInt(",");
+    std::cout << "Day 6, puzzle 1: " << getLeastFuelCommonPosition(initialStatePositions, false) << std::endl;
+
+    assert(getLeastFuelCommonPosition(initialStatePositionsTest, true) == 168);
+    std::cout << "Day 6, puzzle 2: " << getLeastFuelCommonPosition(initialStatePositions, true) << std::endl;
+}
