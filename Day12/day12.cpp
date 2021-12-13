a5946510103a515abb4482f778d1131e72cfa1c3
Arco Gelderblom <arco.gelderblom@hotmail.com>
2021-12-13 10:10:05 +0100

    Made day 12 run substantially quicker by changing std::vector to std::set because of faster erase operation

--- Day12/day12.cpp
+++ Day12/day12.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <map>
+#include <set>
 
 #include "inputfile.hpp"
 #include "stringoperations.hpp"
 #include "vectoroperations.hpp"
 
-std::vector< std::vector<std::string> > findAllPaths(std::vector< std::vector<std::string> > roughMap, bool visitSmallCaveTwice = false)
+std::set< std::vector<std::string> > findAllPaths(std::vector< std::vector<std::string> > roughMap, bool visitSmallCaveTwice = false)
 {
     std::map< std::string, std::vector<std::string> > options;
     for (std::vector<std::string> entry : roughMap)
@@ -26,18 +27,18 @@ std::vector< std::vector<std::string> > findAllPaths(std::vector< std::vector<st
         }
     }
     
-    std::vector< std::vector<std::string> > paths;
-    std::vector< std::vector<std::string> > unfinishedPaths;
+    std::set< std::vector<std::string> > paths;
+    std::set< std::vector<std::string> > unfinishedPaths;
     std::vector<std::string> startPath = {"start"};
-    unfinishedPaths.push_back(startPath);
+    unfinishedPaths.insert(startPath);
     while (unfinishedPaths.size() > 0)
     {
+        std::set< std::vector<std::string> > oldPaths = paths;
         std::vector< std::vector<std::string> > deadEnds;
         std::vector< std::vector<std::string> > unfinishedPathsToAdd;
-        int currentPathToHandle = 0;
-        for (std::vector<std::string>& path : unfinishedPaths)
+        std::set< std::vector<std::string> > unfinishedPathsCopy = unfinishedPaths;
+        for (const std::vector<std::string> & path : unfinishedPathsCopy)
         {
-            currentPathToHandle++;
             std::string currentPosition = path.back();
             if (options[currentPosition].size() > 1)
             {
@@ -45,17 +46,9 @@ std::vector< std::vector<std::string> > findAllPaths(std::vector< std::vector<st
                 // must create copies
                 for (int i = 1; i < options[currentPosition].size(); i++)
                 {
-                    if (std::all_of(options[currentPosition][i].begin(), options[currentPosition][i].end(), [](unsigned char c){ return std::isupper(c); }))
-                    {
-                        copies[i - 1].push_back(options[currentPosition][i]);
-                        unfinishedPathsToAdd.push_back(copies[i-1]);
-                    }
-                    else if (std::find(copies[i-1].begin(), copies[i-1].end(), options[currentPosition][i]) == copies[i-1].end())
-                    {
-                        copies[i - 1].push_back(options[currentPosition][i]);
-                        unfinishedPathsToAdd.push_back(copies[i-1]);
-                    }
-                    else if (visitSmallCaveTwice && std::count(path.begin(), path.end(), options[currentPosition][i]) == 1 && std::count_if(smallCaves.begin(), smallCaves.end(), [&](std::string destination){ return std::count(path.begin(), path.end(), destination) == 2; }) == 0 && std::find(smallCaves.begin(), smallCaves.end(), options[currentPosition][i]) != smallCaves.end())
+                    if (std::all_of(options[currentPosition][i].begin(), options[currentPosition][i].end(), [](unsigned char c){ return std::isupper(c); }) ||
+                        std::find(copies[i-1].begin(), copies[i-1].end(), options[currentPosition][i]) == copies[i-1].end() ||
+                        (visitSmallCaveTwice && std::count(path.begin(), path.end(), options[currentPosition][i]) == 1 && std::count_if(smallCaves.begin(), smallCaves.end(), [&](std::string destination){ return std::count(path.begin(), path.end(), destination) == 2; }) == 0 && std::find(smallCaves.begin(), smallCaves.end(), options[currentPosition][i]) != smallCaves.end()))
                     {
                         copies[i - 1].push_back(options[currentPosition][i]);
                         unfinishedPathsToAdd.push_back(copies[i-1]);
@@ -63,31 +56,31 @@ std::vector< std::vector<std::string> > findAllPaths(std::vector< std::vector<st
                 }
             }
 
-            if (std::all_of(options[currentPosition][0].begin(), options[currentPosition][0].end(), [](unsigned char c){ return std::isupper(c); }))
+            if (std::all_of(options[currentPosition][0].begin(), options[currentPosition][0].end(), [](unsigned char c){ return std::isupper(c); }) ||
+                std::find(path.begin(), path.end(), options[currentPosition][0]) == path.end() ||
+                (visitSmallCaveTwice && std::count(path.begin(), path.end(), options[currentPosition][0]) == 1 && std::count_if(smallCaves.begin(), smallCaves.end(), [&](std::string destination){ return std::count(path.begin(), path.end(), destination) == 2; }) == 0 && std::find(smallCaves.begin(), smallCaves.end(), options[currentPosition][0]) != smallCaves.end()))
             {
-                path.push_back(options[currentPosition][0]);
-            }
-            else if (std::find(path.begin(), path.end(), options[currentPosition][0]) == path.end())
-            {
-                path.push_back(options[currentPosition][0]);
-            }
-            else if (visitSmallCaveTwice && std::count(path.begin(), path.end(), options[currentPosition][0]) == 1 && std::count_if(smallCaves.begin(), smallCaves.end(), [&](std::string destination){ return std::count(path.begin(), path.end(), destination) == 2; }) == 0 && std::find(smallCaves.begin(), smallCaves.end(), options[currentPosition][0]) != smallCaves.end())
-            {
-                path.push_back(options[currentPosition][0]);
+                std::vector<std::string> tmp = path;
+                unfinishedPaths.erase(path);
+                tmp.push_back(options[currentPosition][0]);
+                unfinishedPaths.insert(tmp);
             }
             else 
             {
-                path.push_back(options[currentPosition][0]);
-                deadEnds.push_back(path);
+                std::vector<std::string> tmp = path;
+                unfinishedPaths.erase(path);
+                tmp.push_back(options[currentPosition][0]);
+                unfinishedPaths.insert(tmp);
+                deadEnds.push_back(tmp);
             }
         }
         
-        unfinishedPaths.insert(unfinishedPaths.end(), unfinishedPathsToAdd.begin(), unfinishedPathsToAdd.end());
+        unfinishedPaths.insert(unfinishedPathsToAdd.begin(), unfinishedPathsToAdd.end());
         
         // remove dead ends
         for (const std::vector<std::string> &  deadEnd : deadEnds)
         {
-            unfinishedPaths.erase(std::remove(unfinishedPaths.begin(), unfinishedPaths.end(), deadEnd), unfinishedPaths.end());
+            unfinishedPaths.erase(deadEnd);
         }
 
         // check for finished paths
@@ -97,15 +90,15 @@ std::vector< std::vector<std::string> > findAllPaths(std::vector< std::vector<st
             if (path.back() == "end")
             {
                 //finished path
-                paths.push_back(path);
+                paths.insert(path);
             }
         }
         
-        if (paths.size() > oldPathsSize)
+        for (auto path : paths)
         {
-            for (int i = oldPathsSize; i < paths.size(); i++)
+            if (oldPaths.find(path) == oldPaths.end()) // path is new
             {
-                unfinishedPaths.erase(std::remove(unfinishedPaths.begin(), unfinishedPaths.end(), paths[i]), unfinishedPaths.end());
+                unfinishedPaths.erase(path);
             }
         }
     }
@@ -117,10 +110,10 @@ int main(void)
 {
     std::vector< std::vector<std::string> > testInput1 = VectorOperations::splitVectorInSubVectors(StringOperations::splitString("start-A\nstart-b\nA-c\nA-b\nb-d\nA-end\nb-end", "\n"), "-");
     assert(findAllPaths(testInput1).size() == 10);
-
+    
     std::vector< std::vector<std::string> > testInput2 = VectorOperations::splitVectorInSubVectors(StringOperations::splitString("dc-end\nHN-start\nstart-kj\ndc-start\ndc-HN\nLN-dc\nHN-end\nkj-sa\nkj-HN\nkj-dc", "\n"), "-");
     assert(findAllPaths(testInput2).size() == 19);
-
+    
     std::vector< std::vector<std::string> > testInput3 = VectorOperations::splitVectorInSubVectors(StringOperations::splitString("fs-end\nhe-DX\nfs-he\nstart-DX\npj-DX\nend-zg\nzg-sl\nzg-pj\npj-he\nRW-he\nfs-DX\npj-RW\nzg-RW\nstart-pj\nhe-WI\nzg-he\npj-fs\nstart-RW", "\n"), "-");
     assert(findAllPaths(testInput3).size() == 226);
     
